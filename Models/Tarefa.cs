b5cb5f52000ae3fc4cf257f6eb8a1935c7fbb98a
Rychardsson <rychardssong2@gmail.com>
2025-07-23 23:11:16 -0300

    Implement TarefaController methods and add validation attributes to Tarefa model

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,18 @@ namespace TrilhaApiDesafio.Models
     public class Tarefa
     {
         public int Id { get; set; }
+        
+        [Required(ErrorMessage = "O título é obrigatório")]
+        [StringLength(200, ErrorMessage = "O título deve ter no máximo 200 caracteres")]
         public string Titulo { get; set; }
+        
+        [StringLength(1000, ErrorMessage = "A descrição deve ter no máximo 1000 caracteres")]
         public string Descricao { get; set; }
+        
+        [Required(ErrorMessage = "A data é obrigatória")]
         public DateTime Data { get; set; }
+        
+        [Required(ErrorMessage = "O status é obrigatório")]
         public EnumStatusTarefa Status { get; set; }
     }
 }
